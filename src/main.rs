8ff95241acfa3d2337b5132f44f4d7800f7d57c5
Valera6 <v79166789533@gmail.com>
2024-05-30 10:32:28 +0000

    docs: _

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,41 @@
 #![allow(clippy::get_first)]
 #![allow(clippy::len_zero)]
-use clap::{ArgGroup, Args, Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
+use v_utils::io::ExpandedPath;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
 	#[command(subcommand)]
 	command: Commands,
+	/// Where the books are stored
+	#[arg(long, default_value = "~/.booktyping")]
+	library: ExpandedPath,
 }
 #[derive(Subcommand)]
 enum Commands {
-	/// Hello the world or rust (flags are mutually exclusive)
+	/// Starts the thing
 	///Ex
 	///```sh
-	///what hello -w "!"
+	///booktyping run -w "!"
 	///```
-	Hello(HelloArgs),
+	Run(RunArgs),
 }
 
-#[derive(Args)]
-#[command(group(
-    ArgGroup::new("channel")
-        .required(true)
-        .args(&["world", "rust"]),
-))]
-struct HelloArgs {
+#[derive(Clone, Debug, Default, derive_new::new, Args)]
+struct RunArgs {
 	/// Hello to world
 	#[arg(short, long)]
-	world: bool,
-	/// Hello to rust
-	#[arg(short, long)]
-	rust: bool,
+	web: bool,
 
-	/// Message to send after hello
-	after_hello_message: Vec<String>,
+	book: String,
 }
 
 fn main() {
 	let cli = Cli::parse();
 	match cli.command {
-		Commands::Hello(args) => {
-			let hello_target = match (args.world, args.rust) {
-				(true, false) => "World",
-				(false, true) => "Rust",
-				(true, true) => panic!("Cannot hello two things"),
-				(false, false) => panic!("Specify what to hello"),
-			};
-
-			let message = format!("Hello, {hello_target}{}", &args.after_hello_message.join(""));
-			println!("{message}");
+		Commands::Run(args) => {
+			dbg!(&args);
 		}
 	}
 }
